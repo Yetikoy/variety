9370c2bc788d0ae18762fc9f6e27def7a703b0e9
Roman Kuzmichev <yetikotuch@yandex.ru>
2023-06-24 10:36:39 +0000

    update utils

--- src/util.py
+++ src/util.py
@@ -3,3 +3,12 @@ import operator
 import itertools
 
 flatten = itertools.chain.from_iterable
+
+def group_by(items, key=lambda x:x):
+    if isinstance(items, dict):
+        items = items.items()
+    result = defaultdict(list)
+    for k,v in items:
+        result[key(v)].append(k)
+    return result
+
