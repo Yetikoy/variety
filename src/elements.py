7fa25c7c006e71631bcbbfba8885c61751a1fac6
Roman Kuzmichev <yetikotuch@yandex.ru>
2023-06-25 05:08:25 +0000

    add convenience to elements

--- src/elements.py
+++ src/elements.py
@@ -8,11 +8,11 @@ class NamedWithPrefix:
 
 class Element(NamedWithPrefix):
     def get_prefix(self): return 'Undefined'
-    def __init__(self, counts, value, name='', is_supernode=False):
+    def __init__(self, counts, value, name='', is_supernode=False, lenses=tuple()):
         super().__init__(self.get_prefix(), counts, name)
         self.value = value
         self.is_supernode = is_supernode
-        self.lenses = [['Voltage', lambda v, state: v]]
+        self.lenses = [['Voltage', lambda v, state: v]] + list(lenses)
 
     def get_initial_state(self): return None
     def get_current(self, v, state):
@@ -34,18 +34,19 @@ class Source(Element):
             constant_voltage = voltage
             voltage = lambda t: constant_voltage
         super().__init__(counts, voltage, name, is_supernode=True)
-
     def get_initial_state(self): return 0 # tracks time
     def get_mismatch(self, v, state):
         expected_voltage = self.value(state)
         return expected_voltage - v
     def commit(self, v, i, state, dt): return state+dt
-
-
-
+    
 class Inductor(Element):
+    def __init__(self, counts, value, name='', initial_current=0):
+        super().__init__(counts, value, name)
+        self.lenses.append(['Current', lambda v, state: state[0]])
+        self.initial_current = initial_current
     def get_prefix(self): return 'L'
-    def get_initial_state(self): return [0, 0.000000000001]
+    def get_initial_state(self): return [self.initial_current, 0]
     def get_current(self, v, state):
         I, dt = state
         dI = dt * v / self.value
@@ -63,9 +64,18 @@ class Diode(Element):
             return 0
 
 class Capacitor(Element):
+    def __init__(self, counts, value, name='', initial_charge=0):
+        super().__init__(counts, value, name, is_supernode=True)
+        self.lenses.append(['Charge', lambda v, state: state])
+        self.get_initial_state = lambda: initial_charge
+    def get_prefix(self): return 'C'
+    def commit(self, v, i, state, dt):
+        #print('C0:', v, i, state)
+        return state + i * dt
+    def get_mismatch(self, v, state): return self.value * state - v    
+    
+class Jumper(Element):
     def __init__(self, counts, value, name=''):
         super().__init__(counts, value, name, is_supernode=True)
-    def get_prefix(self): return 'C'
-    def get_initial_state(self): return 0
-    def commit(self, v, i, state, dt): return state + i * dt
-    def get_mismatch(self, v, state): return self.value * state - v
+    def get_prefix(self): return 'J'
+    def get_mismatch(self, v, state): return v
